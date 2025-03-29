020430efa4018337175c98662fdf5ad4a63dd68d
BushDemon <159964570+BushDemon@users.noreply.github.com>
2025-03-30 00:44:25 +0100

    feat: Added DTOs: CreateCourseDto, CreatePeriodDto (separated for POST and GET because of cycling)

---
+++ backend/List.Server/Data/DTOs/CourseReadDto.cs
@@ -0,0 +1,9 @@
+namespace List.Server.Data.DTOs
+{
+    public class CourseReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PeriodName { get; set; } = string.Empty;
+    }
+}
---
+++ backend/List.Server/Data/DTOs/PeriodCreateDto.cs
@@ -0,0 +1,7 @@
+namespace List.Server.Data.DTOs
+{
+    public class PeriodCreateDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
