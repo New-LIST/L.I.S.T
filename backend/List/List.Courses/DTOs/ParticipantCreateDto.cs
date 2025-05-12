cbd13d4a9d456da906c22846184db34f3c75c800
Ondrej Babinský <babinskyondrej@gmail.com>
2025-05-12 05:58:07 +0200

    add participant endpoints for getting users courses, joining course, get participants by course, approve/remove participant

---
+++ backend/List/List.Courses/DTOs/ParticipantCreateDto.cs
@@ -0,0 +1,6 @@
+namespace List.Courses.DTOs;
+
+public class ParticipantCreateDto
+{
+    public int CourseId { get; set; }
+}
---
+++ backend/List/List.Courses/DTOs/ParticipantUpdateDto.cs
@@ -0,0 +1,7 @@
+namespace List.Courses.DTOs;
+
+public class ParticipantUpdateDto
+{
+    public int CourseId { get; set; }
+    public int UserId { get; set; }
+}
